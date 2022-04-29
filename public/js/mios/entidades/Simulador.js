85de331b5e6061eb91cef5c6168e75f5870c62b4
Pablo Adrian Gonzalez <etereosum@gmail.com>
2022-04-29 12:00:07 -0500

    ADD: se agrega el simulador en la solicitud

---
+++ public/js/mios/entidades/Simulador.js
@@ -0,0 +1,13 @@
+class Simulador {
+  constructor(payload) {
+    this.productoId = payload.ProductoId || "";
+    this.tipoVehiculoId = payload.tipoVehiculoId || "";
+    this.modelo = payload.modelo || "";
+    this.cilindraje = payload.cilindraje || "";
+    this.periodo = payload.periodo || "";
+    this.numCuotas = payload.numCuotas || "";
+    this.precio = payload.precio || "";
+    this.valorCuota = payload.valorCuota || "";
+  }
+}
+
