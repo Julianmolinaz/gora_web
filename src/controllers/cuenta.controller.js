40ebd1b977e32b1d0b029c57c108e3e367aa97e3
Pablo Adrian Gonzalez <etereosum@gmail.com>
2022-07-09 20:47:36 -0500

    ADD: se crea la vista de detalle de solicitud

--- src/controllers/cuenta.controller.js
+++ src/controllers/cuenta.controller.js
@@ -3,6 +3,10 @@ class CuentaController {
   static index(req, res) {
     return res.render("cuenta/index.html");
   }
+
+  static show(req, res) {
+    return res.render("cuenta/solicitud/show.html");
+  }
 }
 
 module.exports = CuentaController;
--- src/routes/web/cuenta.js
+++ src/routes/web/cuenta.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const CuentaController = require("../../controllers/cuenta.controller");
 
 router.get("/", CuentaController.index);
+router.get("/:solicitudId", CuentaController.show);
 
 module.exports = router;
