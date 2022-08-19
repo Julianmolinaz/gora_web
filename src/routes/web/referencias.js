7de85b8efb6499fd14d002f936beb7d09a75bc3f
Pablo Adrian Gonzalez <etereosum@gmail.com>
2022-08-18 19:01:33 -0500

    ADD: se adiciona informacion de los terceros en el show de la solicitud

--- src/routes/web/referencias.js
+++ src/routes/web/referencias.js
@@ -6,4 +6,7 @@ const router = express.Router();
 
 router.get("/create/:solicitudId", authorization, ReferenciaController.create);
 
+router.get("/edit/:solicitudId", authorization, ReferenciaController.edit);
+
+
 module.exports = router;
