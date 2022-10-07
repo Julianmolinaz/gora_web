0379a764d8b9f5c65c480d246f226c62d5cb3d91
etereosum <etereosum@gmail.com>
2022-10-06 23:57:47 -0500

    ADD: modificaciones desde el servidor

--- src/database/conexiones/local.js
+++ src/database/conexiones/local.js
@@ -3,6 +3,7 @@ const mysql = require("mysql2");
 const conexion = mysql.createConnection({
   host: process.env.LOCAL_DB_HOST,
   user: process.env.LOCAL_DB_USER,
+  port: process.env.LOCAL_DB_PORT,
   database: process.env.LOCAL_DB_NAME,
   password: process.env.LOCAL_DB_PASS
 });
