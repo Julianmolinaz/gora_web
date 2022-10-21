b8bfddfa73536a68da193368496e0a61c8363a3f
Pablo Adrian Gonzalez <etereosum@gmail.com>
2022-10-21 17:24:37 -0500

    BUG: se resetean los campos de contacto al cambiar de departamento

--- src/database/repositories/contactos.repository.js
+++ src/database/repositories/contactos.repository.js
@@ -5,16 +5,17 @@ class ContactosRepository {
   static getDepartamentosPresencia() {
     return new Promise((resolve, reject) => {
       const query = `
-	SELECT departamentos.*
-	FROM contactos
-	INNER JOIN municipios ON contactos.municipio_id = municipios.id
-	INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
-	GROUP BY departamentos.id
-	ORDER BY departamentos.nombre
+        SELECT departamentos.*
+        FROM contactos
+        INNER JOIN municipios ON contactos.municipio_id = municipios.id
+        INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
+        WHERE contactos.estado = 1
+        GROUP BY departamentos.id
+        ORDER BY departamentos.nombre
       `;
       conexion.query(query, (error, result) => {
-	if (error) reject(error);
-	resolve(result);
+        if (error) reject(error);
+        resolve(result);
       });
     });
   }
@@ -22,17 +23,18 @@ class ContactosRepository {
   static getCiudadesConPresencia(departamentoId) {
     return new Promise((resolve, reject) => {
       const query = `
-	SELECT municipios.* 
-	FROM contactos
-	INNER JOIN municipios ON contactos.municipio_id = municipios.id
-	INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
-	WHERE departamentos.id = ${departamentoId}
-	GROUP BY municipios.id
-	ORDER BY municipios.nombre
+        SELECT municipios.* 
+        FROM contactos
+        INNER JOIN municipios ON contactos.municipio_id = municipios.id
+        INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
+        WHERE departamentos.id = ${departamentoId}
+        AND contactos.estado = 1
+        GROUP BY municipios.id
+        ORDER BY municipios.nombre
       `;
       conexion.query(query, (error, result) => {
-	if (error) reject(error);
-	resolve(result);
+      if (error) reject(error);
+        resolve(result);
       });
     });
   }
@@ -40,20 +42,21 @@ class ContactosRepository {
   static getContactosPorCiudad(ciudadId) {
     return new Promise((resolve, reject) => {
       const query = `
-	SELECT 
-	  contactos.*,
-	  municipios.nombre as municipio_nombre,
-	  departamentos.id as departamento_id,
-	  departamentos.nombre as departamento_nombre
-	FROM contactos
-	INNER JOIN municipios ON contactos.municipio_id = municipios.id
-	INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
-	WHERE contactos.municipio_id = ${ciudadId}
-	ORDER BY contactos.nombre
+        SELECT 
+          contactos.*,
+          municipios.nombre as municipio_nombre,
+          departamentos.id as departamento_id,
+          departamentos.nombre as departamento_nombre
+        FROM contactos
+        INNER JOIN municipios ON contactos.municipio_id = municipios.id
+        INNER JOIN departamentos ON municipios.departamento_id = departamentos.id
+        WHERE contactos.municipio_id = ${ciudadId}
+        AND contactos.estado = 1
+        ORDER BY contactos.nombre
       `;
       conexion.query(query, (error, result) => {
-	if (error) reject(error);
-	resolve(result);
+      if (error) reject(error);
+        resolve(result);
       });
     });
   }
